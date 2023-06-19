d33923a7ea98cde802ff8cc43abb0f4e7807467e
Mrk0p <sachinwele23@gamil.com>
2023-06-19 20:30:38 +0530

    Added:switch case practice problems

---
+++ src/Switch_Case/VowelOrConsonant.java
@@ -0,0 +1,33 @@
+package Switch_Case;
+
+import java.util.Scanner;
+
+public class VowelOrConsonant{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a alphabet :");
+
+        char ch = scanner.next().charAt(0);
+        switch (ch) {
+            case 'a':
+                System.out.println(ch+" is vowel");
+                break;
+            case 'e':
+                System.out.println(ch+" is vowel");
+                break;
+            case 'i':
+                System.out.println(ch+" is vowel");
+                break;
+            case 'o':
+                System.out.println(ch+" is vowel");
+                break;
+            case 'u':
+                System.out.println(ch+" is vowel");
+                break;
+            default:
+                System.out.println(ch+" is consonant");
+        }
+    }
+
+}
